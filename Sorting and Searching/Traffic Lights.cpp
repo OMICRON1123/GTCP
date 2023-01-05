3474a497b1845f1be553ef500df61e73ed5933ac
Juan <jprietoc@unal.edu.co>
2023-01-05 11:53:37 -0500

    Comments on Traffic Lights

--- Sorting and Searching/Traffic Lights.cpp
+++ Sorting and Searching/Traffic Lights.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Para este ejercicio se tiene un set en el que se guardan las posiciones en las que
+se han puesto semáforos y un multiset donde se guarda las longitudes de los
+intervalos entre los semáforos. En el set se guarda originalmente las posiciones 0
+y x, mientras que en el multiset originalmente se guarda la longitud inicial,
+es decir x. Cada vez que se añade un semáforo se analiza qué semáforo había adelante
+y atrás. Digamos que el semáforo de adelante está en la posición b y el de atrás en
+la posición a. Entonces, se borra del multiset un intervalo de longitud b-a
+mientras que se añaden dos intervalos uno b-p y otro p-a donde p representa la
+posición en la que se está añadiendo un semáforo actualmente. Finalmente, se añade
+p al set y se observa cuál es la longitud más larga en el multiset.
+*/
+
 int main() {
     int x,n,p;
     set<int> light;
